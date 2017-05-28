3cf0e1c40010b163f53af6f14a9a5cdb64785f13
Cristian Schuszter <darkgeeky@gmail.com>
2017-05-28 13:56:03 +0300

    Stuff

--- app/com/hacktm/websockets/StressCalculatorActor.scala
+++ app/com/hacktm/websockets/StressCalculatorActor.scala
@@ -11,12 +11,15 @@ import play.api.libs.json.{Json, Reads, Writes}
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
 case class CalculateStressNotification(carJsons: List[CarJsonDTO])
+
 case class StressPayload(event: String, payload: Int)
+
 object StressPayload {
   implicit val readsCarJson: Reads[StressPayload] = Json.reads[StressPayload]
   implicit val writesCarJson: Writes[StressPayload] = Json.writes[StressPayload]
 
 }
+
 object StressCalculatorActor {
   def props(out: ActorRef, id: Int) = Props(new StressCalculatorActor(out, id))
 
@@ -28,15 +31,15 @@ class StressCalculatorActor(out: ActorRef, id: Int) extends Actor {
     case CalculateStressNotification(carJsons) =>
       val futureCarPos = carJsons.take(700)
       val currentCarPos = carJsons.drop(700)
-      val distances  = currentCarPos.zip(futureCarPos).map {
-        case  (past, present) =>
+      val distances = currentCarPos.zip(futureCarPos).map {
+        case (past, present) =>
           println("Calculating stress")
 
-          val xDiff = past.trafficXPos.zip(present.trafficXPos).map { case (l1, l2) => (l1 - l2) * (l1 - l2)}
-          val yDiff = past.trafficYPos.zip(present.trafficYPos).map { case (l1, l2) => (l1 - l2) * (l1 - l2)}
-          val dist = xDiff.zip(yDiff).map { case (x,y) => Math.sqrt(x+y) }
-          val speedXDiff = past.trafficXSpeed.zip(present.trafficXSpeed).map { case (l1, l2) => Math.abs(l1-l2)}
-          val speedYDiff= past.trafficYSpeed.zip(present.trafficYSpeed).map { case (l1, l2) => Math.abs(l1-l2)}
+          val xDiff = past.trafficXPos.zip(present.trafficXPos).map { case (l1, l2) => (l1 - l2) * (l1 - l2) }
+          val yDiff = past.trafficYPos.zip(present.trafficYPos).map { case (l1, l2) => (l1 - l2) * (l1 - l2) }
+          val dist = xDiff.zip(yDiff).map { case (x, y) => Math.sqrt(x + y) }
+          val speedXDiff = past.trafficXSpeed.zip(present.trafficXSpeed).map { case (l1, l2) => Math.abs(l1 - l2) }
+          val speedYDiff = past.trafficYSpeed.zip(present.trafficYSpeed).map { case (l1, l2) => Math.abs(l1 - l2) }
           val mySpeedDiff = Math.abs(past.speed - present.speed)
 
           println(s"Computed distances $dist")
@@ -44,16 +47,18 @@ class StressCalculatorActor(out: ActorRef, id: Int) extends Actor {
       }.toSeq
 
       val stressFactorSpeed = distances.map(_._2).sum / distances.length
-      val stressFactor: Int = if(stressFactorSpeed > 25) {
+      val stressFactor: Int = if (stressFactorSpeed < 10) {
         10
-      } else if(stressFactorSpeed > 35) {
+      } else if (stressFactorSpeed < 20) {
+        15
+      } else if (stressFactorSpeed < 35) {
         25
-      } else if(stressFactorSpeed > 50){
-        60
+      } else if (stressFactorSpeed < 50) {
+        40
       } else {
-        80
+        65
       }
-//      distances._1.map(dist => dist.count(d => d <= 2.0))
+      //      distances._1.map(dist => dist.count(d => d <= 2.0))
       out ! StressPayload("stress", stressFactor)
   }
 }
